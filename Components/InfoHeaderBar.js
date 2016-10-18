a328036e6cd6056d271224f0a4c7dbe1c17bb644
Jeff Woo <cheeseonhead@gmail.com>
2016-10-18 01:11:56 -0400

    Added some temp views to see how it would look

--- Components/InfoHeaderBar.js
+++ Components/InfoHeaderBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+  StyleSheet,
   Text,
   View
 } from 'react-native';
@@ -7,15 +8,40 @@ import {
 class InfoHeaderBar extends Component {
   render() {
     return (
-      <View style={{
-        borderRadius: 10,
-        margin: 10,
-        height: 100,
-        backgroundColor: 'blue'
-      }}>
+      <View style={styles.InfoHeaderBar}>
+        <View style={{
+          flex:1,
+          flexDirection: 'row',
+          backgroundColor: 'purple',
+          alignItems: 'center'
+        }}>
+          <Text style={{
+            fontFamily: 'Supercell-Magic',
+            color: 'white'
+          }}>139</Text>
+          <Text style={{
+            fontFamily: 'Supercell-Magic',
+            color: 'white'
+          }}>angryNeeson123</Text>
+        </View>
+        <View style={{
+          flex:1.5,
+          backgroundColor: 'aqua'
+        }} />
       </View>
     )
   }
 }
 
 export default InfoHeaderBar;
+
+const styles=StyleSheet.create({
+  InfoHeaderBar: {
+    flexDirection: 'column',
+    borderRadius: 10,
+    margin: 5,
+    padding: 10,
+    height: 100,
+    backgroundColor: 'blue'
+  }
+});
